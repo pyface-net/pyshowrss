60a18ec5674874fcbfd77bbe64ae3794a1e7c3ff
pyface-net <pyface-net@users.noreply.github.com>
2016-03-07 14:04:47 -0500

    updated icon

--- pyinstaller.bat
+++ pyinstaller.bat
@@ -1,1 +1,2 @@
-pyinstaller.exe pyshowrss.py --onefile --distpath .\dist\windows
+c:\Python27\Scripts\pyinstaller.exe pyshowrss.py --onefile --distpath .\dist\windows -i .\icons\feed-icon.ico
+copy pyshowrss.ini .\dist\windows
