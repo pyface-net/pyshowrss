da727c0c26f45c63a1c6e9b4fca11590aedb84d8
pyface-net <pyface-net@users.noreply.github.com>
2016-03-10 20:20:48 -0500

    pep8

--- pyshowrss.py
+++ pyshowrss.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-__author__ = "pyface-net"
-__version__ = "0.1"
-__license__ = "MIT License"
-
 import argparse
 import subprocess
 import ConfigParser
@@ -13,21 +9,29 @@ import urllib
 import feedparser
 import traceback
 
+__author__ = "pyface.net"
+__version__ = "0.1"
+__license__ = "MIT License"
+
+
 def log(message):
         print message
 
-def logExceptionStackTrace(message):
+
+def log_exception(message):
     log(message)
-    with open("exceptions.log",'a') as outfile:
+    with open("exceptions.log", 'a') as outfile:
         outfile.write(message + "\n")
         outfile.write(traceback.format_exc())
 
-def saveCache(cache, cache_file):
+
+def save_cache(cache, cache_file):
     with open(cache_file, 'w+') as outfile:
         cache.write(outfile)
         outfile.close()
 
-def loadCreateCacheFile(cache_file):
+
+def load_create_cache_file(cache_file):
     cache = None
     if cache_file:
         cache = ConfigParser.ConfigParser()
@@ -38,17 +42,19 @@ def loadCreateCacheFile(cache_file):
             pass
     return cache
 
+
 def is_valid(buffer):
     return (buffer.index('d8') == 0)
 
-def downloadTorrentFile(url, output_dir, validate):
+
+def download_torrent_file(url, output_dir, validate):
     safe_url = urllib.quote(url, safe="%/:=&?~#+!$,;'@()*[]")
     torrent_name = safe_url.split('/')[-1]
     response = urllib2.urlopen(safe_url)
     torrent_data = response.read()
 
     if not validate or (validate and is_valid(torrent_data)):
-        torrent_file = os.path.join(output_dir,torrent_name)
+        torrent_file = os.path.join(output_dir, torrent_name)
         with open(torrent_file, 'w') as outfile:
             outfile.write(torrent_data)
         return True
@@ -56,8 +62,8 @@ def downloadTorrentFile(url, output_dir, validate):
         return False
 
 
-def processRSSFeed(url, output_dir, post_dl_cmd, validate, cache_file):
-    cache = loadCreateCacheFile(cache_file)
+def process_rss_feed(url, output_dir, post_dl_cmd, validate, cache_file):
+    cache = load_create_cache_file(cache_file)
     rss_feed = feedparser.parse(url)
     log("Checking for new shows...")
     for item in rss_feed.entries:
@@ -76,56 +82,57 @@ def processRSSFeed(url, output_dir, post_dl_cmd, validate, cache_file):
 
         if download:
             try:
-                log("Downloading show: '%s' (%s)" % (title,torrent_link))
-                if downloadTorrentFile(torrent_link, output_dir, validate):
+                log("Downloading show: '%s' (%s)" % (title, torrent_link))
+                if download_torrent_file(torrent_link, output_dir, validate):
                     if post_dl_cmd:
                         subprocess.check_call(post_dl_cmd.split() + [title.encode('ascii', 'ignore')] + [torrent_link])
                     if cache:
                         cache.set("cache", cache_key, "True")
-                        saveCache(cache,cache_file)
+                        save_cache(cache, cache_file)
                 else:
-                    log("Download failed: '%s' (%s)" % (title,torrent_link))
+                    log("Download failed: '%s' (%s)" % (title, torrent_link))
             except:
-                logExceptionStackTrace("ERROR: Exception while downloading: '%s' (%s)" % (title,torrent_link))
+                log_exception("ERROR: Exception while downloading: '%s' (%s)" % (title, torrent_link))
     log("Done!")
 
 
-def getRSSUrl(config):
-    url = config.get("showrss","url")
-    user_id = config.get("showrss","user_id")
-    quality = config.get("showrss","quality")
-    repack = config.get("showrss","repack")
+def get_rss_url(config):
+    url = config.get("showrss", "url")
+    user_id = config.get("showrss", "user_id")
+    quality = config.get("showrss", "quality")
+    repack = config.get("showrss", "repack")
 
-    return (url % (user_id,quality,repack))
+    return (url % (user_id, quality, repack))
 
 
-def getOptions(config):
+def get_options(config):
     post_dl_cmd = None
     cache_file = None
     validate = False
 
-    if config.has_option("default","post_dl_cmd"):
-        post_dl_cmd = config.get("default","post_dl_cmd")
-    if config.has_option("default","cache_file"):
-        cache_file = config.get("default","cache_file")
-    if config.has_option("default","validate_torrent_file"):
-        validate = config.getboolean("default","validate_torrent_file")
+    if config.has_option("default", "post_dl_cmd"):
+        post_dl_cmd = config.get("default", "post_dl_cmd")
+    if config.has_option("default", "cache_file"):
+        cache_file = config.get("default", "cache_file")
+    if config.has_option("default", "validate_torrent_file"):
+        validate = config.getboolean("default", "validate_torrent_file")
 
     return (post_dl_cmd, validate, cache_file)
 
 
-def _getArgs():
-    DEFAULT_CONFIG_FILENAME = "pyshowrss.ini"
+def get_args():
+    default_config_file = "pyshowrss.ini"
     program = os.path.basename(__file__).split('.')[0]
     arg_parser = argparse.ArgumentParser(prog=program, version=__version__,
                                          description="Downloads .torrent files from a ShowRSS feed")
     arg_parser.add_argument('-c', '-config', dest='configFile',
-                            default=os.path.join(os.getcwd(),DEFAULT_CONFIG_FILENAME),
+                            default=os.path.join(os.getcwd(), default_config_file),
                             help="path to configuration file to use "
-                                 "(default = ./" + DEFAULT_CONFIG_FILENAME + ")")
-    arg_parser.add_argument('-o', '-outputDir', dest='outputDir', default=os.path.join(os.getcwd(), "torrents"),
+                            "(default = ./" + default_config_file + ")")
+    arg_parser.add_argument('-o', '-outputDir', dest='outputDir',
+                            default=os.path.join(os.getcwd(), "torrents"),
                             help="full path to an output directory for .torrent files "
-                                 "(default = ./torrents)")
+                            "(default = ./torrents)")
 
     args = arg_parser.parse_args()
 
@@ -139,14 +146,15 @@ def _getArgs():
 
     return args
 
+
 def main():
-    args = _getArgs()
+    args = get_args()
     if args:
         config = ConfigParser.ConfigParser()
         config.read(args.configFile)
-        (post_dl_cmd, validate, cache_file) = getOptions(config)
-        url = getRSSUrl(config)
-        processRSSFeed(url, args.outputDir, post_dl_cmd, validate, cache_file)
+        (post_dl_cmd, validate, cache_file) = get_options(config)
+        url = get_rss_url(config)
+        process_rss_feed(url, args.outputDir, post_dl_cmd, validate, cache_file)
 
 if __name__ == "__main__":
     main()
